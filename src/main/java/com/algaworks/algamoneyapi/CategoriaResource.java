81efcdb4f25944b6d313b896e3eba4da5e32d102
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-01-23 04:16:42 -0400

    CategoriaRepository e CategoriaResource - Listar categorias

---
+++ src/main/java/com/algaworks/algamoneyapi/CategoriaResource.java
@@ -0,0 +1,26 @@
+package com.algaworks.algamoneyapi;
+
+import com.algaworks.algamoneyapi.model.Categoria;
+import com.algaworks.algamoneyapi.repository.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    @GetMapping
+    public ResponseEntity<?> listar(){
+        List<Categoria> categorias = categoriaRepository.findAll();
+        return categorias.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(categorias);
+    }
+    
+}
