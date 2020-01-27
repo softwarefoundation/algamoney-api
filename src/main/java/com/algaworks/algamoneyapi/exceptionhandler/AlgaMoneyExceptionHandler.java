5ad47871fa02f3f920fb1247c69eef9f419a4d15
Dherkyan Ribeiro <dherkyan@gmail.com>
2020-01-27 14:19:21 -0400

    3.10 - Bean Validation - @NotNull

--- src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgaMoneyExceptionHandler.java
+++ src/main/java/com/algaworks/algamoneyapi/exceptionhandler/AlgaMoneyExceptionHandler.java
@@ -7,9 +7,15 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @ControllerAdvice
 public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
@@ -19,9 +25,26 @@ public class AlgaMoneyExceptionHandler extends ResponseEntityExceptionHandler {
 
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String mensagem = messageSource.getMessage("mensagem.invalida",null, LocaleContextHolder.getLocale());
+        String mensagem = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
         String stacktrace = ex.getCause().toString();
-        return  handleExceptionInternal(ex,new Erro(mensagem,stacktrace),headers, HttpStatus.BAD_REQUEST, request);
+        List<Erro> erros = Arrays.asList(new Erro(mensagem, stacktrace));
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<Erro> erros = criarListaDeErros(ex.getBindingResult());
+        return handleExceptionInternal(ex, erros, headers, HttpStatus.BAD_REQUEST, request);
+    }
+
+    private List<Erro> criarListaDeErros(BindingResult bindResult) {
+        List<Erro> erros = new ArrayList<>();
+        bindResult.getFieldErrors().forEach(f -> {
+            String msg = messageSource.getMessage(f, LocaleContextHolder.getLocale());
+            erros.add(new Erro(msg, f.toString()));
+        });
+
+        return erros;
     }
 
     public class Erro {
